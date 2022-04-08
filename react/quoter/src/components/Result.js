a7f3a89e536320e1cd395196a8b6f034be208a33
iOznny <f000170014@gmail.com>
2022-04-08 01:04:07 -0500

    Changes

--- react/quoter/src/components/Result.js
+++ react/quoter/src/components/Result.js
@@ -6,7 +6,7 @@ const Result = ({ amount, limit, total }) => (
         <p>Pagar en: { limit } meses.</p>
         <p>Cantidad Solicitada: $ { amount }</p>
         <p>Pago Mensual: $ { (total / limit).toFixed(2) }</p>
-        <p>Total: $ { total } </p>
+        <p>Total: $ { (total).toFixed(2) } </p>
     </div>    
 );
  
