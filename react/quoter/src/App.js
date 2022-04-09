805d49296620d09a775ea15b0b8073b6b5d6dec3
iOznny <f000170014@gmail.com>
2022-04-08 21:39:11 -0500

    Changes in React

--- react/quoter/src/App.js
+++ react/quoter/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
+
+// Components
+import Form from './components/Form.js';
+import Header from "./components/Header.js";
 
 function App() {
+
+  // Define state
+  const [amount, setAmount] = useState(0);
+  const [limit, setLimit] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header title="Cotizador de Prestamos" />
+      
+      <div className='container'>
+        <Form 
+          amount={ amount } 
+          setAmount={ setAmount }
+          limit={ limit }
+          setLimit={ setLimit }
+        />
+
+      </div>
+    </Fragment>
   );
 }
 
